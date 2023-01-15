e3c07a7865edfc4f117059104285b3b19324b078
Ashish <mudgil.ashish27@gmail.com>
2023-01-15 11:29:10 +0530

    initial commit

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,30 @@
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { CartContext } from '../../contexts/cart.context.jsx';
+
+import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
+import { CartDropdownContainer, EmptyMessage , CartItems } from './cart-dropdown.styles';
+
+const CartDropdown =() => {
+    const { cartItems } = useContext(CartContext);
+    const navigate = useNavigate();
+
+    const goToCheakOutHandler = () =>{
+        navigate('/checkout');
+    }
+    return (
+        <CartDropdownContainer>
+            <CartItems>
+                {
+                    cartItems.length?(cartItems.map( (item)=> <CartItem key={item.id} cartItem={item} />))
+                    :( <EmptyMessage>Your Cart Is Empty</EmptyMessage>)
+                }
+            </CartItems>
+            <Button onClick={goToCheakOutHandler}>GO TO CHECKOUT</Button>
+        </CartDropdownContainer>
+    );
+};
+
+export default CartDropdown;
